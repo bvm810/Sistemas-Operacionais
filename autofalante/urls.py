be4b987e3bc649dc9ec8ef3ec47f0ddd2119da8d
Bernardo Miranda <bvm@poli.ufrj.br>
2018-06-04 00:32:00 -0300

    V2 do site, agora com Login e Logout

--- autofalante/urls.py
+++ autofalante/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'autofalante'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:playlist_id>/<int:load>/', views.detail, name='detail'),
-    path('fila/<int:playlist_id>/<int:musica_arduino_id>/', views.sendLine, name='sendLine'),
-    path('comandos/<int:playlist_id>/<int:musica_arduino_id>/', views.sendCommand, name='sendCommand'),
+    path('login/', auth_views.login, {'template_name': 'autofalante/login.html'}, name='login'),
+    path('logout/', auth_views.logout, {'next_page': '/autofalante'}, name='logout'),
+    path('home/', views.home, name='home'),
+    path('home/<int:playlist_id>/<int:load>/', views.detail, name='detail'),
+    path('home/fila/<int:playlist_id>/<int:musica_arduino_id>/', views.sendLineHome, name='sendLineHome'),
+    path('fila/<int:musica_arduino_id>/', views.sendLineIndex, name='sendLineIndex'),
+    path('home/comandos/<int:playlist_id>/<int:musica_arduino_id>/', views.sendCommand, name='sendCommand'),
     path('esp/fila/', views.getLine, name='getLine'),
     path('esp/comandos/', views.getCommand, name='getCommand'),
 ]
--- autofalante/views.py
+++ autofalante/views.py
@@ -11,7 +11,6 @@ COMMANDS_PLAYLIST_ID = 6
 LOAD_ARDUINO_ID = 9992
 
 def index(request):
-    created_playlists = Playlist.objects.exclude(id = COMMANDS_PLAYLIST_ID)
     musicas = Musica.objects.exclude(playlist = COMMANDS_PLAYLIST_ID)
     songs = []
     for musica in musicas:
@@ -24,10 +23,28 @@ def index(request):
                 songs.append(s)            
     template = loader.get_template('autofalante/index.html')
     context = {
-        'created_playlists': created_playlists,
         'songs': songs,
     }
     return render(request,'autofalante/index.html',context)
+
+def home(request):
+    created_playlists = Playlist.objects.exclude(id = COMMANDS_PLAYLIST_ID)
+    musicas = Musica.objects.exclude(playlist = COMMANDS_PLAYLIST_ID)
+    songs = []
+    for musica in musicas:
+        if (Musica.objects.filter(arduino_id = musica.arduino_id).count()==1):
+            s = Musica.objects.get(arduino_id = musica.arduino_id)
+            songs.append(s)
+        else:
+            s = Musica.objects.filter(arduino_id = musica.arduino_id)[0]
+            if (s.arduino_id != songs[len(songs)-1].arduino_id):
+                songs.append(s)            
+    template = loader.get_template('autofalante/home.html')
+    context = {
+        'created_playlists': created_playlists,
+        'songs': songs,
+    }
+    return render(request,'autofalante/home.html',context)
 
 def detail(request, playlist_id, load):
     playlist = Playlist.objects.get(id = playlist_id)
@@ -60,11 +77,23 @@ def sendCommand(request, playlist_id, musica_arduino_id):
         musica.execute = True
         musica.save()
     if (playlist_id == 0):
-        return index(request)
+        return home(request)
     else:    
         return detail(request,playlist_id,0)
 
-def sendLine(request, playlist_id, musica_arduino_id):
+def sendLineIndex(request, musica_arduino_id):
+    comando = Musica.objects.get(arduino_id = 9992)
+    if (comando.execute == False):
+        comando.execute = True
+        comando.save()
+    m = Musica.objects.filter(arduino_id = musica_arduino_id)
+    musica = m[0]
+    if (musica.in_line == False):
+        musica.in_line = True
+        musica.save()
+    return index(request)
+
+def sendLineHome(request, playlist_id, musica_arduino_id):
     comando = Musica.objects.get(arduino_id = 9992)
     if (comando.execute == False):
         comando.execute = True
@@ -75,9 +104,9 @@ def sendLine(request, playlist_id, musica_arduino_id):
         musica.in_line = True
         musica.save()
     if (playlist_id == 0):
-        return index(request)
+        return home(request)
     else:    
-        return detail(request,playlist_id,0)
+        return detail(request,playlist_id,0)        
 
 def getCommand(request):
     musica_line = Musica.objects.filter(execute = True).order_by('modified_at')
